285c6e00da4438ef19a56700f7092259a7ac7984
Vishal Modi <vishalrmodi@gmail.com>
2024-02-06 22:57:22 -0800

    Update movieExist

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,6 +2,13 @@ const service = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function movieExists(req, res, next) {
+  if (!req.params.movieId) {
+    return next({
+      status: 404,
+      message: `Movie cannot be found without movie id`,
+    });
+  }
+
   const movie = await service.read(req.params.movieId);
 
   if (movie) {
@@ -18,7 +25,7 @@ async function read(request, res) {
 
 async function list(req, res) {
   const { is_showing } = req.query;
-  const is_showing_flag = is_showing === 'true' ? true : false;
+  const is_showing_flag = is_showing === "true" ? true : false;
   const data = await service.list(is_showing_flag);
   res.json({ data });
 }
