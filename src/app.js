98c517f2f5a6f22c99e6b799378f42660608fec9
Vishal Modi <vishalrmodi@gmail.com>
2024-02-06 21:28:53 -0800

    Reorder cors

--- src/app.js
+++ src/app.js
@@ -2,15 +2,16 @@ if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const moviesRouter = require("./movies/movies.router");
-const theatersRouter = require("./theaters/theaters.router");
-const reviewsRouter = require("./reviews/reviews.router");
 const cors = require("cors");
 
 // configure CORS
 app.use(cors())
 // Handle preflight requests
 app.options('*', cors());
+
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
 app.use(express.json());
 
